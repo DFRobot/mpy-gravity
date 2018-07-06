9ee5f07d0e63fb01650db73f6928045ebe493293
mengbishu <841616394@qq.com>
2018-07-06 15:07:04 +0800

    v0.1

--- PYBoard/BME280/bme280Demo.py
+++ PYBoard/BME280/bme280Demo.py
@@ -21,15 +21,15 @@ import pyb,time
 import bme280
 
 i2c=pyb.I2C(1,pyb.I2C.MASTER,baudrate=100000)
-bmp=BME280_I2C(i2c)
+bme=BME280_I2C(i2c)
 #spi = pyb.SPI(1, pyb.SPI.MASTER, baudrate=100000, polarity=0, phase=0)
 #cs  = Pin("X1", Pin.OUT_OD)
-#bmp = BME280_SPI(spi,cs)
+#bme = BME280_SPI(spi,cs)
 
 while True:
-  print("Temp : %s *C" %bmp.getTemp())
-  print("Pres : %s Pa" %bmp.getPress())
-  print("Alti : %s m"  %bmp.getAltitude())
-  print("Humi : %s  "  %bmp.getHumidity())
+  print("Temp : %s *C" %bme.getTemp())
+  print("Pres : %s Pa" %bme.getPress())
+  print("Alti : %s m"  %bme.getAltitude())
+  print("Humi : %s  "  %bme.getHumidity())
   print("")
   time.sleep(1)
